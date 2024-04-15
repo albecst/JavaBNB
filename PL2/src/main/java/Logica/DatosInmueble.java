ce0dc7e6ea608745b0027ebb3fb4ee75862f8fae
AlbeTroll <albertocastilloAC05@gmail.com>
2024-04-16 01:51:55 +0200

    Update: Reducción de código

--- PL2/src/main/java/Logica/DatosInmueble.java
+++ PL2/src/main/java/Logica/DatosInmueble.java
@@ -1,8 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logica;
+
+/**
+ *
+ * @author alber
+ */
 public class DatosInmueble {
     
     //Atributos
