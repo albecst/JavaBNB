db5e2bef6763ebbd8920b0eef13780e301d2b401
AlbeTroll <albertocastilloAC05@gmail.com>
2024-04-16 01:51:48 +0200

    Update: Reducción de código

--- PL2/src/main/java/Logica/Cliente.java
+++ PL2/src/main/java/Logica/Cliente.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logica;
 
 /**
@@ -18,7 +14,6 @@ public class Cliente {
     private String telefono;
     
     //Constructor
-
     public Cliente(String dni, String nombre, String correo, String clave, String telefono) {
         this.dni = dni;
         this.nombre = nombre;
