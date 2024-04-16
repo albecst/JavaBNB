136dd7ce85a2f46c5c434fdafbd18b3b9d50e46a
AlbeTroll <albertocastilloAC05@gmail.com>
2024-04-17 00:28:22 +0200

    Update: 3 nuevos atributos Docs: Documentación constructor Add: Método comprobarExistenciaCliente, registrarAnfitrión, registrarParticular Remove: método registrarCliente

--- PL2/src/main/java/Logica/InicioSesion.java
+++ PL2/src/main/java/Logica/InicioSesion.java
@@ -1,5 +1,6 @@
 package Logica;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
@@ -11,39 +12,68 @@ public class InicioSesion {
     //Atributos
     private Administrador administrador;
     private Anfitrion anfitrion;
+    private Particular particular;
     private ArrayList<Cliente> clientes;
-
+    private ArrayList<Anfitrion> anfitriones;
+    private ArrayList<Particular> particulares;
 
     /**
-     * Constructor
+     * Constructor de la clase InicioSesion.
      *
      * @param administrador
      * @param anfitrion
+     * @param particular
      * @param cliente
      */
-    public InicioSesion(Administrador administrador, Anfitrion anfitrion, Cliente cliente) {
+    public InicioSesion(Administrador administrador, Anfitrion anfitrion, Cliente cliente, Particular particular) {
         this.administrador = administrador;
         this.anfitrion = anfitrion;
+        this.particular = particular;
         this.clientes = new ArrayList<>();
     }
 
-    public void registrarCliente(String dni, String nombre, String correo, String clave, String telefono) {
+    public boolean comprobarExistenciaCliente (String correo, String dni, String telefono){
+        boolean statement = false;
         for (Cliente cliente : clientes) {
             if (cliente.getCorreo().equals(correo)) {
                 System.out.println("Este correo ya existe");
-                return;
+                statement = true;
+                break;
             }
             else if (cliente.getDni().equals(dni)){
                 System.out.println("Este DNI ya existe");
-                return;
+                statement = true;
+                break;
             }
             else if (cliente.getTelefono().equals(telefono)){
                 System.out.println("Este teléfono ya existe");
-                return;
+                statement = true;
+                break;
             }
         }
-        Cliente nuevoCliente = new Cliente(dni, nombre, correo, clave, telefono);
-        clientes.add(nuevoCliente);
+        return statement;
+    }
+
+
+    //Aquí preguntaríamos al usuario si quiere registrarse como anfitrión o particular.
+    public void registrarAnfitrion(LocalDate fechaRegistro, String dni, String nombre, String correo, String clave, String telefono) {
+        if (comprobarExistenciaCliente(correo, dni, telefono)) {
+            return;
+        }
+        Anfitrion nuevoAnfitrion = new Anfitrion(fechaRegistro, dni, nombre, correo, clave, telefono);
+        clientes.add(nuevoAnfitrion);
+        anfitriones.add(nuevoAnfitrion);
+    }
+
+    public void registrarParticular(Tarjeta tarjetaCredito, boolean vip, String dni, String nombre, String correo, String clave, String telefono) {
+        for (Cliente cliente : clientes) {
+            if (comprobarExistenciaCliente(correo, dni, telefono)) {
+                return;
+            }
+            Particular nuevoParticular = new Particular(tarjetaCredito, vip, dni, nombre, correo, clave, telefono);
+            clientes.add(nuevoParticular);
+            particulares.add(nuevoParticular);
+        }
     }
 
     public void iniciarSesion(String correo, String clave) {
