31652496f17690ae7da81c6e211c50471341ca9b
AlbeTroll <albertocastilloAC05@gmail.com>
2024-04-17 00:39:02 +0200

    New: nueva clase Reserva, con 4 atributos y Getters & Setters

---
+++ PL2/src/main/java/Logica/Reserva.java
@@ -0,0 +1,100 @@
+package Logica;
+
+import java.time.LocalDate;
+
+public class Reserva {
+
+    //Atributos
+    private Inmueble inmueble;
+    private Particular particular;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+
+    /**
+     * Constructor de la clase Reserva.
+     *
+     * @param particular  Particular que realiza la reserva.
+     * @param inmueble    Inmueble que se reserva.
+     * @param fechaInicio Fecha de inicio de la reserva.
+     * @param fechaFin    Fecha de fin de la reserva.
+     */
+    public Reserva(Particular particular, Inmueble inmueble, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.inmueble = inmueble;
+        this.particular = particular;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * Getters & Setters
+     *
+     * Get the value of fechaFin
+     */
+    public Inmueble getInmueble() {
+        return inmueble;
+    }
+
+    /**
+     * Set the value of inmueble
+     *
+     * @param inmueble new value of inmueble
+     */
+    public void setInmueble(Inmueble inmueble) {
+        this.inmueble = inmueble;
+    }
+
+    /**
+     * Get the value of particular
+     *
+     * @return the value of particular
+     */
+    public Particular getParticular() {
+        return particular;
+    }
+
+    /**
+     * Set the value of particular
+     *
+     * @param particular new value of particular
+     */
+    public void setParticular(Particular particular) {
+        this.particular = particular;
+    }
+
+    /**
+     * Get the value of fechaInicio
+     *
+     * @return the value of fechaInicio
+     */
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * Set the value of fechaInicio
+     *
+     * @param fechaInicio new value of fechaInicio
+     */
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * Get the value of fechaFin
+     *
+     * @return the value of fechaFin
+     */
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * Set the value of fechaFin
+     *
+     * @param fechaFin new value of fechaFin
+     */
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
