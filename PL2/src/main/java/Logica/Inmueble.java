0448f76563f28eb298bf210ba2d8afa19914c2e1
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-05-12 03:05:37 +0200

    Update: cambios en el constructor y método 'toString()' añaddo en "Inmueble"

--- PL2/src/main/java/Logica/Inmueble.java
+++ PL2/src/main/java/Logica/Inmueble.java
@@ -5,54 +5,36 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Inmueble implements Serializable {
-    
+
     private String titulo;
     private Direccion direccion;
     private DatosInmueble datosInmueble;
     private String tipo; //Casa o apartamento
     private double precioNoche;
-    private ArrayList <String> servicios = new ArrayList<>();
+    private String servicios;
     private double calificacion; //Si es menor a 0 o mayor a 5 error
     private String fotografia; //Será un String, porque la fotografía no deja de ser una ruta dentro de nuestro dispositivo.
     private LocalDate fechaInicioReserva;
     private LocalDate fechaFinReserva;
-    
+    private String descripcion;
+
     //Constructor
-    public Inmueble(String titulo, Direccion direccion, DatosInmueble datosInmueble, String tipo, double precioNoche, double calificacion, String fotografia, LocalDate fechaInicioReserva, LocalDate fechaFinReserva) {
+    public Inmueble(String titulo, String descripcion, Direccion direccion, DatosInmueble datosInmueble, String tipo, double precioNoche, String fotografia, String servicios) {
         this.titulo = titulo;
         this.direccion = direccion;
         this.datosInmueble = datosInmueble;
         this.tipo = tipo;
         this.precioNoche = precioNoche;
-        this.calificacion = calificacion;
+        this.calificacion = 0;
         this.fotografia = fotografia;
-        this.fechaInicioReserva = fechaInicioReserva;
-        this.fechaFinReserva = fechaFinReserva;
-    }
-
-
-    /**
-     * Método para añadir un servicio al inmueble.
-     * @param servicio Servicio a añadir.
-     */
-    public void añadirServicio(String servicio){
-        if(!servicios.contains(servicio)){
-            servicios.add(servicio);
-        }
+        this.servicios = servicios;
+        this.descripcion = descripcion;
     }
 
     /**
-     * Método para eliminar un servicio del inmueble.
-     * @param servicio Servicio a eliminar.
-     */
-    public void eliminarServicio(String servicio){
-        if(servicios.contains(servicio)){
-            servicios.remove(servicio);
-        }
-    }
-
-    /**
-     * Método para comprobar si un inmueble está disponible en unas fechas concretas.
+     * Método para comprobar si un inmueble está disponible en unas fechas
+     * concretas.
+     *
      * @param fechaEntrada Fecha de entrada.
      * @param fechaSalida Fecha de salida.
      * @return True si está disponible, false si no lo está.
@@ -61,46 +43,10 @@ public class Inmueble implements Serializable {
         return fechaInicioReserva.isAfter(fechaSalida) || fechaFinReserva.isBefore(fechaEntrada);
     }
 
-
     /**
-     * Getters & Setters
+     * Getters & Setters /
      *
-     * Get the value of fechaInicioReserva
-     *
-     * @return the value of fechaInicioReserva
-     */
-    public LocalDate getFechaInicioReserva() {
-        return fechaInicioReserva;
-    }
-
-    /**
-     * Set the value of fechaInicioReserva
-     *
-     * @param fechaInicioReserva new value of fechaInicioReserva
-     */
-    public void setFechaInicioReserva(LocalDate fechaInicioReserva) {
-        this.fechaInicioReserva = fechaInicioReserva;
-    }
-
-    /**
-     * Get the value of fechaFinReserva
-     *
-     * @return the value of fechaFinReserva
-     */
-    public LocalDate getFechaFinReserva() {
-        return fechaFinReserva;
-    }
-
-    /**
-     * Set the value of fechaFinReserva
-     *
-     * @param fechaFinReserva new value of fechaFinReserva
-     */
-    public void setFechaFinReserva(LocalDate fechaFinReserva) {
-        this.fechaFinReserva = fechaFinReserva;
-    }
-
-    /**
+     * /**
      * Get the value of fotografía
      *
      * @return the value of fotografía
@@ -118,7 +64,6 @@ public class Inmueble implements Serializable {
         this.fotografia = fotografia;
     }
 
-    
     /**
      * Get the value of calificacion
      *
@@ -136,8 +81,7 @@ public class Inmueble implements Serializable {
     public void setCalificacion(double calificacion) {
         if (calificacion < 0 || calificacion > 5) {
             throw new IllegalArgumentException("La calificación debe estar entre 0 y 5.");
-        }
-        else{
+        } else {
             this.calificacion = calificacion;
         }
     }
@@ -147,7 +91,7 @@ public class Inmueble implements Serializable {
      *
      * @return the value of servicios
      */
-    public ArrayList<String> getServicios() {
+    public String getServicios() {
         return servicios;
     }
 
@@ -169,7 +113,6 @@ public class Inmueble implements Serializable {
         this.precioNoche = precioNoche;
     }
 
-
     /**
      * Get the value of tipo
      *
@@ -187,7 +130,6 @@ public class Inmueble implements Serializable {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-
 
     /**
      * Get the value of datosInmueble
@@ -207,7 +149,6 @@ public class Inmueble implements Serializable {
         this.datosInmueble = datosInmueble;
     }
 
-
     /**
      * Get the value of direccion
      *
@@ -226,7 +167,6 @@ public class Inmueble implements Serializable {
         this.direccion = direccion;
     }
 
-
     /**
      * Get the value of titulo
      *
@@ -244,4 +184,20 @@ public class Inmueble implements Serializable {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Inmueble{" + "titulo=" + titulo + ", direccion=" + direccion + ", datosInmueble=" + datosInmueble + ", tipo=" + tipo + ", precioNoche=" + precioNoche + ", servicios=" + servicios + ", calificacion=" + calificacion + ", fotografia=" + fotografia + ", fechaFinReserva=" + fechaFinReserva + ", descripcion=" + descripcion + '}';
+    }
+    
+
+
 }
