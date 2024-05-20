c70cf850d17b223f02de25f0b111c19ae6234649
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-05-21 01:35:52 +0200

    Update: método para cargas las reservas al darle al botón "Mis reservas"

--- PL2/src/main/java/UI_UX/MainScreenClient.java
+++ PL2/src/main/java/UI_UX/MainScreenClient.java
@@ -713,8 +713,8 @@ public class MainScreenClient extends javax.swing.JPanel {
     }//GEN-LAST:event_logoActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+        deleteBuildings();
+        Aplicacion.loadGuestCheckReserves();    }//GEN-LAST:event_jButton3ActionPerformed
 
     private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
         filterComboBox.setSelectedItem("Filtrar por:");
