8c4db6e05f83514dce97a6606baabd955d409dfe
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-05-21 21:32:23 +0200

    Update: eliminamos el ArrayList de inmuebles para llevarlo a 'JavaBNB' + método "setSuperAnfitrion" funcionando + método "añadirInmueble()"

--- PL2/src/main/java/Logica/Anfitrion.java
+++ PL2/src/main/java/Logica/Anfitrion.java
@@ -2,37 +2,19 @@ package Logica;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import Logica.JavaBNB;
 
 public class Anfitrion extends Cliente {
-   
+
     private LocalDate fechaRegistro;
     private boolean superAnfitrion;
-    private ArrayList<Inmueble> inmuebles = new ArrayList<>();
 
-    
     public Anfitrion(String dni, String nombre, String correo, String clave, String telefono) {
         super(dni, nombre, correo, clave, telefono);
         this.fechaRegistro = LocalDate.now();
-    }
-    
-    
-
-    /**
-     * Getters & Setters
-     *
-     * Get the value of inmuebles
-     */
-    public ArrayList<Inmueble> getInmuebles() {
-        return this.inmuebles;
+        this.superAnfitrion = false;
     }
 
-    public void addInmuebles(Inmueble inmueble) {
-        this.inmuebles.add(inmueble);
-        this.setSuperAnfitrion();
-    }
-     
-    
-    
     /**
      * Get the value of superanfitrion
      *
@@ -46,20 +28,27 @@ public class Anfitrion extends Cliente {
      * Set the value of superanfitrion
      */
     public void setSuperAnfitrion() {
-        int calificacion=0;
-        int cantidad=0;
-        for (Inmueble i : this.inmuebles) {
-           calificacion+=i.getCalificacion();
-           cantidad++;
+        int calificacion = 0;
+        int cantidad = 0;
+        for (Inmueble i : JavaBNB.getInmuebles()) {
+            if (i.getCliente().getDni().equals(i.getCliente().getDni())) {
+                calificacion += i.getCalificacion();
+                cantidad++;
+            }
+            int notamedia = calificacion / cantidad;
+            if (notamedia >= 4) {
+                this.superAnfitrion = true;
+            } else {
+                this.superAnfitrion = false;
+            }
         }
-        int notamedia = calificacion/cantidad;
-        if (notamedia>=4)
-            this.superAnfitrion = true;
-        else
-            this.superAnfitrion = false;
     }
 
-    
+    public void añadirInmueble(Inmueble inmueble) {
+        JavaBNB.añadirInmueble(inmueble);
+        JavaBNB.guardarDatos();
+    }
+
     /**
      * Get the value of fechaRegistro
      *
@@ -77,14 +66,10 @@ public class Anfitrion extends Cliente {
     public void setFechaRegistro(LocalDate fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
-    
-    
 
     @Override
     public String toString() {
         return super.toString() + ", fecha de registro:" + fechaRegistro + ", Super anfitrión:" + superAnfitrion;
     }
 
-    
-    
 }
