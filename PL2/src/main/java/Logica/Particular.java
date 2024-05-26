77f08d4a1fea481f941092ea28d43823fde764f9
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-05-26 16:38:07 +0200

    Docs: documentación de la clase 'Particular'

--- PL2/src/main/java/Logica/Particular.java
+++ PL2/src/main/java/Logica/Particular.java
@@ -1,24 +1,26 @@
 package Logica;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
+/**
+ * La clase Particular representa un cliente particular que utiliza la plataforma JavaBNB.
+ * Hereda de la clase Cliente e incluye detalles adicionales como una tarjeta de crédito y el estado VIP.
+ */
 public class Particular extends Cliente implements Serializable {
 
     private Tarjeta tarjetaCredito;
     private boolean vip;
-    //private ArrayList<Reserva> reservas = new ArrayList<>();
 
     /**
-     * Constructor de la clase Particular
+     * Constructor de la clase Particular.
      *
-     * @param tarjetaCredito
-     * @param vip
-     * @param dni
-     * @param nombre
-     * @param correo
-     * @param clave
-     * @param telefono
+     * @param tarjetaCredito la tarjeta de crédito del cliente
+     * @param vip            indica si el cliente es VIP
+     * @param dni            el DNI del cliente
+     * @param nombre         el nombre del cliente
+     * @param correo         el correo electrónico del cliente
+     * @param clave          la clave del cliente
+     * @param telefono       el teléfono del cliente
      */
     public Particular(Tarjeta tarjetaCredito, boolean vip, String dni, String nombre, String correo, String clave, String telefono) {
         super(dni, nombre, correo, clave, telefono);
@@ -27,28 +29,28 @@ public class Particular extends Cliente implements Serializable {
     }
 
     /**
-     * Método para disminuir el saldo de la tarjeta de crédito
+     * Método para disminuir el saldo de la tarjeta de crédito.
+     * Si el cliente es VIP, se aplica un descuento del 10%.
      *
-     * @param cantidad
-     * @return
+     * @param cantidad la cantidad a disminuir del saldo
+     * @return un mensaje con el saldo actual
      */
     public String disminuirSaldo(double cantidad) {
         Double saldo = tarjetaCredito.getSaldo();
         if (vip) {
             saldo -= cantidad * 0.9;
-            tarjetaCredito.setSaldo(saldo);
         } else {
             saldo -= cantidad;
-            tarjetaCredito.setSaldo(saldo);
         }
+        tarjetaCredito.setSaldo(saldo);
         return "El saldo actual es: " + saldo + "€";
     }
 
     /**
-     * Método para aumentar el saldo de la tarjeta de crédito
+     * Método para aumentar el saldo de la tarjeta de crédito.
      *
-     * @param cantidad
-     * @return
+     * @param cantidad la cantidad a aumentar al saldo
+     * @return un mensaje con el saldo actual
      */
     public String aumentarSaldo(double cantidad) {
         Double saldo = tarjetaCredito.getSaldo();
@@ -58,86 +60,66 @@ public class Particular extends Cliente implements Serializable {
     }
 
     /**
-    
-    public void eliminarReserva(Reserva reserva) {
-        this.reservas.remove(reserva);
-    }
-
-    public void addReserva(Reserva reserva) {
-        if (!reservas.contains(reserva)) {
-            reservas.add(reserva);
-            disminuirSaldo(reserva.calcularPrecioTotal());
-        } else {
-            System.out.println("La reserva ya existe");
-        }
-    }
-*/
-    
-    /**
-     * Getters & Setters
-     */
-    //TODO: esto no sobra?
-    /**
-     * Get the value of saldo
+     * Obtiene el saldo de la tarjeta de crédito.
      *
-     * @return the value of saldo
+     * @return el saldo de la tarjeta de crédito
      */
     public double getSaldo() {
         return tarjetaCredito.getSaldo();
     }
 
     /**
-     * Set the value of saldo
+     * Establece el saldo de la tarjeta de crédito.
      *
-     * @param saldo new value of saldo
+     * @param saldo el nuevo saldo de la tarjeta de crédito
      */
     public void setSaldo(double saldo) {
         tarjetaCredito.setSaldo(saldo);
     }
 
     /**
-     * Get the value of vip
+     * Obtiene el estado VIP del cliente.
      *
-     * @return the value of vip
+     * @return true si el cliente es VIP, false en caso contrario
      */
     public boolean isVip() {
         return vip;
     }
 
     /**
-     * Set the value of vip
+     * Establece el estado VIP del cliente.
      *
-     * @param vip new value of vip
+     * @param vip el nuevo estado VIP del cliente
      */
     public void setVip(boolean vip) {
         this.vip = vip;
     }
 
     /**
-     * Get the value of tarjetaCredito
+     * Obtiene la tarjeta de crédito del cliente.
      *
-     * @return the value of tarjetaCredito
+     * @return la tarjeta de crédito del cliente
      */
     public Tarjeta getTarjetaCredito() {
         return tarjetaCredito;
     }
 
     /**
-     * Set the value of tarjetaCredito
+     * Establece la tarjeta de crédito del cliente.
      *
-     * @param tarjetaCredito new value of tarjetaCredito
+     * @param tarjetaCredito la nueva tarjeta de crédito del cliente
      */
     public void setTarjetaCredito(Tarjeta tarjetaCredito) {
         this.tarjetaCredito = tarjetaCredito;
     }
 
-    //public ArrayList<Reserva> getReservas() { return reservas;}
-
-  //  public void setReservas(ArrayList<Reserva> reservas) { this.reservas = reservas;}
-
+    /**
+     * Devuelve una representación en cadena de los datos del cliente particular.
+     *
+     * @return una cadena que representa los datos del cliente particular
+     */
     @Override
     public String toString() {
         return super.toString() + tarjetaCredito.toString() + ", vip:" + vip;
     }
-
 }
