94d2422ffab731e702e7da032b7574f2d5b02322
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-05-26 16:37:19 +0200

    Docs: documentación de la clase 'Anfitrión'

--- PL2/src/main/java/Logica/Anfitrion.java
+++ PL2/src/main/java/Logica/Anfitrion.java
@@ -2,11 +2,25 @@ package Logica;
 
 import java.time.LocalDate;
 
+/**
+ * La clase Anfitrion representa a un cliente que actúa como anfitrión dentro de la
+ * plataforma JavaBNB. Un anfitrión puede registrar inmuebles y recibir una calificación
+ * que le permite convertirse en super anfitrión.
+ */
 public class Anfitrion extends Cliente {
 
     private LocalDate fechaRegistro;
     private boolean superAnfitrion;
 
+    /**
+     * Constructor de la clase Anfitrion.
+     * 
+     * @param dni      el DNI del anfitrión
+     * @param nombre   el nombre del anfitrión
+     * @param correo   el correo electrónico del anfitrión
+     * @param clave    la clave de acceso del anfitrión
+     * @param telefono el número de teléfono del anfitrión
+     */
     public Anfitrion(String dni, String nombre, String correo, String clave, String telefono) {
         super(dni, nombre, correo, clave, telefono);
         this.fechaRegistro = LocalDate.now();
@@ -14,16 +28,17 @@ public class Anfitrion extends Cliente {
     }
 
     /**
-     * Get the value of superanfitrion
+     * Obtiene el valor de superAnfitrion.
      *
-     * @return the value of superanfitrion
+     * @return el valor de superAnfitrion
      */
     public boolean isSuperAnfitrion() {
         return superAnfitrion;
     }
 
     /**
-     * Set the value of superanfitrion
+     * Establece el valor de superAnfitrion en función de la calificación promedio
+     * de los inmuebles registrados por el anfitrión.
      */
     public void setSuperAnfitrion() {
         int calificacion = 0;
@@ -33,41 +48,46 @@ public class Anfitrion extends Cliente {
                 calificacion += i.getCalificacion();
                 cantidad++;
             }
-            int notamedia = calificacion / cantidad;
-            if (notamedia >= 4) {
-                this.superAnfitrion = true;
-            } else {
-                this.superAnfitrion = false;
-            }
         }
+        int notamedia = cantidad > 0 ? calificacion / cantidad : 0;
+        this.superAnfitrion = notamedia >= 4;
     }
 
+    /**
+     * Añade un inmueble a la plataforma JavaBNB y guarda los datos.
+     * 
+     * @param inmueble el inmueble a añadir
+     */
     public void añadirInmueble(Inmueble inmueble) {
         JavaBNB.añadirInmueble(inmueble);
         JavaBNB.guardarDatos();
     }
 
     /**
-     * Get the value of fechaRegistro
+     * Obtiene el valor de fechaRegistro.
      *
-     * @return the value of fechaRegistro
+     * @return el valor de fechaRegistro
      */
     public LocalDate getFechaRegistro() {
         return fechaRegistro;
     }
 
     /**
-     * Set the value of fechaRegistro
+     * Establece el valor de fechaRegistro.
      *
-     * @param fechaRegistro new value of fechaRegistro
+     * @param fechaRegistro el nuevo valor de fechaRegistro
      */
     public void setFechaRegistro(LocalDate fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
 
+    /**
+     * Devuelve una representación en cadena de la información del anfitrión.
+     *
+     * @return una cadena que representa al anfitrión
+     */
     @Override
     public String toString() {
-        return super.toString() + ", fecha de registro:" + fechaRegistro + ", Super anfitrión:" + superAnfitrion;
+        return super.toString() + ", fecha de registro: " + fechaRegistro + ", Super anfitrión: " + superAnfitrion;
     }
-
 }
